73aea2343c0c1762307d8570e43ebb8e82421c0a
Maggie Ward <mlward639@gmail.com>
2021-08-13 21:43:52 -0400

    homeroutes.js added

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -5,7 +5,17 @@ const path = require("path");
 
 // Get HOMEPAGE
 router.get("/", (req, res) => {
-  res.sendFile("/index.html");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+//get EXERCISE page
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 
 module.exports = router;
